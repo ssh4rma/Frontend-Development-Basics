fe25bd282d156caa305378bbf984dc3da720087b
Shubham <shubham.sharma4390@gmail.com>
2025-05-20 17:41:00 +0530

    implemented on() method for handling multiple events

--- jquery/intro.js
+++ jquery/intro.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
-  $("p").click(function () {
-    $(this).hide();
-  });
-});
+// $(document).ready(function () {
+//   $("p").click(function () {
+//     $(this).hide();
+//   });
+// });
 
 $(document).ready(function () {
   $("#p2").mousedown(function () {
@@ -12,4 +12,18 @@ $(document).ready(function () {
 
 $("input").blur(function () {
   $(this).css("background-color", "black");
+});
+
+$(document).ready(function () {
+  $("p").on({
+    mouseenter: function () {
+      $(this).css("background-color", "red");
+    },
+    mouseleave: function () {
+      $(this).css("background-color", "green");
+    },
+    click: function () {
+      $(this).css("background-color", "brown");
+    },
+  });
 });
