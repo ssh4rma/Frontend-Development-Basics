e4f66dab980f5136c3856eebd06dcb0b369b3c02
Shubham <shubham.sharma4390@gmail.com>
2025-05-20 17:33:51 +0530

    implemented focus event method in jquery

--- jquery/intro.js
+++ jquery/intro.js
@@ -1,11 +1,15 @@
-// $(document).ready(function () {
-//   $("p").click(function () {
-//     $(this).hide();
-//   });
-// });
+$(document).ready(function () {
+  $("p").click(function () {
+    $(this).hide();
+  });
+});
 
 $(document).ready(function () {
   $("#p2").mousedown(function () {
     $(this).hide();
   });
+});
+
+$("input").focus(function () {
+  $(this).css("background-color", "black");
 });
