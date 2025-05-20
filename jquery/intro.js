cffe363e2c973f9b8b93add8419bf99d92a76794
Shubham <shubham.sharma4390@gmail.com>
2025-05-20 17:32:14 +0530

    implemented mousedown event method in jquery

--- jquery/intro.js
+++ jquery/intro.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
-  $("p").click(function () {
-    $(this).hide();
-  });
-});
+// $(document).ready(function () {
+//   $("p").click(function () {
+//     $(this).hide();
+//   });
+// });
 
 $(document).ready(function () {
-  $("#p2").mouseenter(function () {
+  $("#p2").mousedown(function () {
     $(this).hide();
   });
 });
