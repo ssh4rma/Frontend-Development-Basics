20aea4bbee730e8159a5cb833ce59168e287970e
Shubham <shubham.sharma4390@gmail.com>
2025-05-21 11:36:14 +0530

    implemented chaining in the jquery

--- jquery/events.js
+++ jquery/events.js
@@ -1,6 +1,1 @@
-$("#stopBtn").click(function () {
-  $("#p2").stop(true, true);
-  $("#p2").animate({ width: "300px", opacity: "0.8" }, "slow");
-  $("#p2").animate({ height: "100px", opacity: "0.4" }, "slow");
-  $("#p2").animate({ width: "100px", opacity: "0.8" }, "slow");
-});
+$("#p2").css("color", "red").slideUp(2000).slideDown(2000);
