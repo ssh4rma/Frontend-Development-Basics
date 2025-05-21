d63e0cfe253afbbd2e11adadf9c373175fdc0397
Shubham <shubham.sharma4390@gmail.com>
2025-05-21 10:47:06 +0530

    implemented stop() method without parameters which will stop first animation in the queue but implement the remaining animations in the queue

--- jquery/events.js
+++ jquery/events.js
@@ -1,6 +1,6 @@
 $("button").click(function () {
   var div = $("#p2");
-  div.animate({ height: "300px", opacity: "0.4" }, "slow");
+  div.stop();
   div.animate({ width: "300px", opacity: "0.8" }, "slow");
   div.animate({ height: "100px", opacity: "0.4" }, "slow");
   div.animate({ width: "100px", opacity: "0.8" }, "slow");
